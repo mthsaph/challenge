e80c062be7cc9c887ad99a4b5e36b6f6f657a85a
dreadfulcreation <104304757+mthsaph@users.noreply.github.com>
2024-10-27 13:16:10 -0300

    Update understat_py.py

--- understat_py.py
+++ understat_py.py
@@ -16,9 +16,9 @@ st.subheader("Filter to any team/player to see all their shots taken!")
 
 client = understatapi.UnderstatClient()
 
-str(league_op) = st.selectbox("Select a league", client.player(player = "11094").leagues, index = None)
+league_op = st.selectbox("Select a league", client.player(player = "11094").leagues, index = None)
 
-league_data = client.league(league_op).get_match_data(season = "2024")
+league_data = client.league(str(league_op)).get_match_data(season = "2024")
 
 team_op = st.selectbox("Select a team", pd.json_normalize(league_data)["a.title"].sort_values().unique(), index = None)
 
