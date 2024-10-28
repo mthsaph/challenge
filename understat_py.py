7b6badb8758b2da040004471dd7eebd3e93f215f
dreadfulcreation <104304757+mthsaph@users.noreply.github.com>
2024-10-28 09:03:15 -0300

    Update understat_py.py

--- understat_py.py
+++ understat_py.py
@@ -30,6 +30,7 @@ def plot_shots(df, ax, pitch):
         
     return pitch
 
+season = "2024"
 client = understatapi.UnderstatClient()
 
 st.title("Understat Data for all leagues available for the 2024 season")
@@ -39,20 +40,18 @@ league_op = st.selectbox("Select a league", client.player(player = "11094").leag
 
 if league_op != None:
     
-    league_data = client.league(league_op).get_match_data(season = "2024")
+    league_data = client.league(league_op).get_match_data(season)
     team_op = st.selectbox("Select a team", pd.json_normalize(league_data)["a.title"].sort_values().unique(), index=None)
 
     if team_op != None:
         
-        team_data = pd.json_normalize(client.team(team_op).get_player_data(season = "2024"))
+        team_data = pd.json_normalize(client.team(team_op).get_player_data(season))
         player_op = st.selectbox("Select a plyer", team_data["player_name"].sort_values().unique(), index=None)
 
         if player_op != None:
             
             player_id = "10552"#team_data[team_data["player_name"] == player_op]["id"][0]
             df = pd.json_normalize(client.player(player = player_id).get_shot_data())
-            df["X"] = df["X"].apply(json.loads)
-            df["Y"] = df["Y"].apply(json.loads)
             pitch = VerticalPitch(pitch_type='statsbomb', line_zorder=2, pitch_color='#f0f0f0', line_color='black', half=True)
             fig, ax = pitch.draw(figsize=(10, 10))
             plot_shots(df, ax, pitch)
