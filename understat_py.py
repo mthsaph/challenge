aa9a690dcd8e434f379920db7bc3349f13115d5c
dreadfulcreation <104304757+mthsaph@users.noreply.github.com>
2024-10-30 15:52:42 -0300

    Update understat_py.py

--- understat_py.py
+++ understat_py.py
@@ -90,7 +90,8 @@ if league_op != None and season != None:
         team_matches = norm_league_data[((norm_league_data["h.title"] == team_op) | (norm_league_data["a.title"] == team_op)) & (norm_league_data["isResult"] == True)]
         team_matches = team_matches[["datetime", "h.title", "a.title", "goals.h", "goals.a"]].reset_index(level=None, drop=True, inplace=False, col_level=0, col_fill="")
         team_matches = team_matches.rename(columns = {"datetime":"DATE AND TIME", "h.title":"HOME TEAM", "a.title":"AWAY TEAM", "goals.h":"HOME GOALS", "goals.a":"AWAY GOALS"})
-        st.table(team_matches)
+        team_matches.index += 1
+        st.table(team_matches.sort(by = "datetime"))
 
         team_data = pd.json_normalize(client.team(team_op).get_player_data(season))
         player_op = st.selectbox("Select a plyer", team_data["player_name"].sort_values().unique(), index=None)
